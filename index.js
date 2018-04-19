710c3f6b8dff72c76d72858f0045968c29d4155f
Christina Anglin <anglin.christina@gmail.com>
2018-04-19 15:46:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much."
+
+function exploringClosures(test) {
+  function innerFunction() {
+    console.log('${test}');
+  }
+  return innerFunction;
+};
+
+exploringClosures('it works');
+
+done();
